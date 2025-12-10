8f9e542eeb5b891fce3f16339ea1eb039bde2af3
Eoin Brereton Hurley <breretoe@tcd.ie>
2025-12-10 22:29:40 +0100

    Ajout des commentaires pour la documentation Sphinx

--- planningpoker/back/main.py
+++ planningpoker/back/main.py
@@ -31,19 +31,53 @@ app.add_middleware(
 
 @app.get("/")
 def on_startup():
+    """
+    Cette fonction exécute l'initialisation de la base de données au lancement du serveur via `initDb()`.
+    
+    """
     # Mettre ici l'appel à la fonciton qui permet de récupérer les tâches
     initDb()
 
 @app.get("/users", response_model=list[schemas.Utilisateur])
 async def getUsers(db: Session = Depends(get_session)):
+    """
+    Cette fonction permet de récupérer tous les utilisateurs présents dans la base de données.
+
+    :param db: Session de connexion à la base de données.
+    :type db: Session
+    :return: Liste des utilisateurs enregistrés.
+    :rtype: list[Utilisateur]
+    
+    """
     return crud.getAllUsers(db)
 
 @app.get("/taches", response_model=list[schemas.Taches])
 async def getTaches(db: Session = Depends(get_session)):
+    """
+    Cette fonction permet de récupérer l'ensemble des tâches stockées dans la base de données.
+
+    :param db: Session de connexion à la base de données.
+    :type db: Session
+    :return: Liste de toutes les tâches.
+    :rtype: list[Tache]
+    
+    """
     return crud.getAllTache(db)
 
 @app.post("/authentification/connexion")
 def connexion(data: schemas.ConnexionInscription, db: Session = Depends(get_session)):
+    """
+    Cette fonction gère l'authentification d'un utilisateur en vérifiant son nom et son mot de passe.
+
+    :param data: Informations de connexion (nom et mot de passe).
+    :type data: ConnexionInscription
+    :param db: Session de connexion à la base.
+    :type db: Session
+    :return: Message de confirmation et informations sur l'utilisateur connecté.
+    :rtype: dict
+    :raises HTTPException: Si l'utilisateur n'existe pas ou si le mot de passe est incorrect.
+    
+    """
     utilisateurs = crud.getAllUsers(db)
     utilisateurDansDB = next(
         (utilisateur for utilisateur in utilisateurs if utilisateur.nom == data.nom and utilisateur.motDePasse == data.motDePasse),
@@ -61,6 +95,18 @@ def connexion(data: schemas.ConnexionInscription, db: Session = Depends(get_sess
 
 @app.post("/authentification/inscription")
 def inscription(data: schemas.ConnexionInscription, db: Session = Depends(get_session)):
+    """
+    Cette fonction gère l'inscription d'un utilisateur si son nom n'existe pas déjà dans la base de données.
+
+    :param data: Informations de connexion (nom et mot de passe).
+    :type data: ConnexionInscription
+    :param db: Session de connexion à la base.
+    :type db: Session
+    :return: Message de succès et informations sur le nouvel utilisateur.
+    :rtype: dict
+    :raises HTTPException: Si le nom est déjà utilisé.
+
+    """
     utilisateurs = crud.getAllUsers(db)
     utilisateursDansDB = next(
         (utilisateur for utilisateur in utilisateurs if utilisateur.nom == data.nom),
@@ -79,11 +125,33 @@ def inscription(data: schemas.ConnexionInscription, db: Session = Depends(get_se
 
 @app.get("/taches/user/{user_id}")
 def getUserTaches(user_id: int, db: Session = Depends(get_session)):
+    """
+    Cette fonction permet de récupérer toutes les tâches que l'utilisateur a créées.
+
+    :param user_id: Identifiant de l'utilisateur.
+    :type user_id: int
+    :param db: Session de connexion à la base.
+    :type db: Session
+    :return: Liste de toutes les tâches associées à l'utilisateur.
+    :rtype: liste[Tache]
+
+    """
     return crud.getTachesWithUserId(db, user_id)
 
 
 @app.post("/taches/creer")
 def creerTache(tache: Taches, db: Session = Depends(get_session)):
+    """
+    Cette fonction permet de créer une tâche. Les attributs du type Tache sont initialisés à partir des informations fournies par l'utilisateur.
+
+   :param tache: Objet contenant les informations fournies par l'utilisateur pour la création d'une tâche.
+   :type tache: Taches
+   :param db: Session de connexion à la base.
+   :type db: Session
+   :return: Message de succès et la nouvelle tâche.
+   :rtype: dict
+
+    """
     nouvelleTache = Tache(
         titre=tache.titre,
         description=tache.description,
@@ -103,6 +171,18 @@ def creerTache(tache: Taches, db: Session = Depends(get_session)):
 
 @app.get("/taches/{tache_id}/createur", response_model=schemas.Utilisateur)
 def getCreateurTache(tache_id: int, db: Session = Depends(get_session)):
+    """
+    Retourne le créateur d'une tâche.
+
+   :param tache_id: Identifiant de la tâche.
+   :type tache_id: int
+   :param db: Session de connexion à la base.
+   :type db: Session
+   :return: Utilisateur créateur de la tâche.
+   :rtype: Utilisateur
+   :raises HTTPException: Si la tâche ou le créateur n'existe pas.
+
+    """
     # Étape 1 : récupérer la tâche
     tache = db.get(Tache, tache_id)
     if not tache:
